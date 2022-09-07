d48a82e44163206183d8303b5070575e5a19f2b2
Lucas Oliveira Lima <lucasoliveirallima@gmail.com>
2022-09-06 23:46:26 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/dsmata/DsmataApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmataApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsmata/DsmataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
